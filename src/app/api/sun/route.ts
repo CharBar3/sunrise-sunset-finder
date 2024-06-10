51847f782be61c93db5254c96d3db4565a74c2ff
Charles Bartlett <ownageunit@gmail.com>
2024-06-10 15:38:30 -0400

    mvp complete

---
+++ src/app/api/sun/route.ts
@@ -0,0 +1,11 @@
+export async function GET(request: Request) {
+  console.log(request);
+
+  // const req = await fetch(
+  //   "https://api.sunrisesunset.io/json?lat=38.907192&lng=-77.036873"
+  // );
+
+  // const data = await req.json();
+
+  // console.log(data);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,119 @@
-import Image from "next/image";
+"use client";
+
+import "leaflet/dist/leaflet.css";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents,
+} from "react-leaflet";
+
+import { Icon, LatLng } from "leaflet";
+import { useState } from "react";
+import { LoaderCircle } from "lucide-react";
 
 export default function Home() {
+  const [sunData, setSunData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const getSunsetSunrise = async (latLng: LatLng) => {
+    setLoading(true);
+
+    try {
+      const req = await fetch(
+        `https://api.sunrisesunset.io/json?lat=${latLng.lat}&lng=${latLng.lng}`
+      );
+      const data = await req.json();
+      console.log(data);
+      setSunData(data.results);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      alert("Error getting sunrise/sunset data");
+    }
+  };
+
+  const customIcon = new Icon({
+    iconUrl:
+      "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLW1hcC1waW4iPjxwYXRoIGQ9Ik0yMCAxMGMwIDYtOCAxMi04IDEycy04LTYtOC0xMmE4IDggMCAwIDEgMTYgMFoiLz48Y2lyY2xlIGN4PSIxMiIgY3k9IjEwIiByPSIzIi8+PC9zdmc+",
+    iconSize: [38, 38],
+  });
+
+  const [position, setPosition] = useState<null | LatLng>(null);
+
+  function MarkCoords() {
+    const map = useMapEvents({
+      click(e) {
+        setPosition(e.latlng);
+        getSunsetSunrise(e.latlng);
+      },
+    });
+
+    return position === null ? null : (
+      <Marker position={position} icon={customIcon}>
+        <Popup>
+          {position ? (
+            <div>
+              <p>Lat: {position.lat}</p>
+              <p>Lng: {position.lng}</p>
+            </div>
+          ) : (
+            ""
+          )}
+        </Popup>
+      </Marker>
+    );
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl flex">
+        Sunset/Sunrise Finder{" "}
+        {loading && <LoaderCircle className="animate-spin" />}
+      </h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        Click on the map to get sunrise and sunset information at the selected
+        location
+      </p>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        Lat {position?.lat}
+      </p>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        Lng {position?.lng}
+      </p>
+      {sunData ? (
+        <ul>
+          <ul>
+            <li>Date: {sunData.date}</li>
+            <li>Dawn: {sunData.dawn}</li>
+            <li>Day Length: {sunData.day_length}</li>
+            <li>Dusk: {sunData.dusk}</li>
+            <li>First Light: {sunData.first_light}</li>
+            <li>Golden Hour: {sunData.golden_hour}</li>
+            <li>Last Light: {sunData.last_light}</li>
+            <li>Solar Noon: {sunData.solar_noon}</li>
+            <li>Sunrise: {sunData.sunrise}</li>
+            <li>Sunset: {sunData.sunset}</li>
+            <li>Timezone: {sunData.timezone}</li>
+            <li>UTC Offset: {sunData.utc_offset}</li>
+          </ul>
+        </ul>
+      ) : (
+        <div></div>
+      )}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <MapContainer
+        center={[39.8283, -98.5795]}
+        zoom={2}
+        className="relaitve min-h-[50vh]"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <MarkCoords />
+      </MapContainer>
+    </div>
   );
 }
